c0021736f6ff340dae3b7fdab5eb6fcc9ab584f1
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-03-02 13:54:03 +0700

    Add simple API routes for testing and demonstration

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('hello', function () {
+    return 'Hello World';
+});
+
+Route::get('time', function () {
+    return date('Y-m-d H:i:s');
+});
