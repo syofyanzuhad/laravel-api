10483da758fc1e3e5f5677a760244c903c6777c1
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2025-03-02 14:36:10 +0700

    Add new API routes for users and JSON response

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,22 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
+Route::get('/users', function () {
+    return App\Models\User::all();
+});
+
 Route::get('hello', function () {
     return 'Hello World';
 });
 
 Route::get('time', function () {
     return date('Y-m-d H:i:s');
+});
+
+Route::get('json', function () {
+    return response()->json([
+        'name' => 'John Doe',
+        'email' => 'user@gmail.com',
+    ]);
 });
